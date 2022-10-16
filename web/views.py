be362162c439a8e7af5680d9fa5f13ab9a32f0d1
TaimurBajwa <112201962+TaimurBajwa@users.noreply.github.com>
2022-10-16 09:56:23 -0500

    quick formatting fix on views

--- web/views.py
+++ web/views.py
@@ -37,12 +37,8 @@ class TrackView(TemplateView):
                 ).get_mapped_data()
                 if data.get('errorMessage') is None:
                     break
-            data['FMP_MAPS_KEY'] = SECRETS['FMP_MAPS_KEY']        
-        return render(
-            request,
-            self.template_name,
-            data,
-        )
+        data['FMP_MAPS_KEY'] = SECRETS['FMP_MAPS_KEY']        
+        return render(request, self.template_name, data)
 
 
 class AboutUsView(TemplateView):
