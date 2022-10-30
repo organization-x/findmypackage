56f2b14fe9648a4dfeed22e9713321d8afa87279
Kaizar R <dhabukro@gmail.com>
2022-10-30 16:25:00 -0400

    Update webscraper.py

--- web/webscraper.py
+++ web/webscraper.py
@@ -1,14 +1,16 @@
+from webbrowser import get
 from bs4 import BeautifulSoup 
 import requests
 
-world_news_response = requests.get("https://apnews.com/hub/world-news") #You can change the url to the news site you want to use (make sure to change the specific tag as well)
-content = world_news_response.text                                      #This just isolates the text in the headline tag from all the actual code
 
-world_soup = BeautifulSoup(content, "lxml") 
+def get_world_headlines():
+    world_news_response = requests.get("https://apnews.com/hub/world-news") #You can change the url to the news site you want to use (make sure to change the specific tag as well)
+    content = world_news_response.text                                      #This just isolates the text in the headline tag from all the actual code
 
-headlines = world_soup.find_all('h2') #Locate Specific Headline Tag
+    world_soup = BeautifulSoup(content, "lxml") 
 
-for headline in headlines:
-    print(headline.text)
+    return world_soup.find_all('h2') #Locate Specific Headline Tag
 
- 
+
+
+    
