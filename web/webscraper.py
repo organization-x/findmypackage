a39717e11b2b7c3502fd5b3f75a96b9c6f78b8dd
Kaizar R <dhabukro@gmail.com>
2022-10-30 16:38:31 -0400

    Created a class for web scraping component

--- web/webscraper.py
+++ web/webscraper.py
@@ -2,15 +2,24 @@ from webbrowser import get
 from bs4 import BeautifulSoup 
 import requests
 
+#p1 = WebScraper()
+#p1.get_world_headlines()
 
-def get_world_headlines():
-    world_news_response = requests.get("https://apnews.com/hub/world-news") #You can change the url to the news site you want to use (make sure to change the specific tag as well)
-    content = world_news_response.text                                      #This just isolates the text in the headline tag from all the actual code
+class WebScraper:
+    def __init__(self):
+        self.headlines_url = 'https://apnews.com/hub/world-news'
 
-    world_soup = BeautifulSoup(content, "lxml") 
 
-    return world_soup.find_all('h2') #Locate Specific Headline Tag
+    def get_world_headlines(self):
+        world_news_response = requests.get(self.headlines_url) #You can change the url to the news site you want to use (make sure to change the specific tag as well)
+        content = world_news_response.text                                      #This just isolates the text in the headline tag from all the actual code
+
+        world_soup = BeautifulSoup(content, "lxml") 
+
+        return world_soup.find_all('h2') #Locate Specific Headline Tag
 
 
 
     
+
+
